177432a95f4593442f70fa34a56045dcbf98b98b
John Shelley <john.shelley.p@gmail.com>
2018-04-25 10:47:32 -0500

    Add FlatList

--- App.js
+++ App.js
@@ -7,6 +7,7 @@
 import React, { Component } from "react";
 import {
   Button,
+  FlatList,
   Image,
   Platform,
   StyleSheet,
@@ -64,7 +65,7 @@ export default class App extends Component {
     this.setState({
       visible: true
     });
-  }
+  };
 
   onDismiss = () => {
     this.setState({
@@ -79,7 +80,28 @@ export default class App extends Component {
           <Text style={styles.welcome}>Welcome to React Native!</Text>
           <Text style={styles.instructions}>To get started, edit App.js</Text>
           <Text style={styles.instructions}>{instructions}</Text>
-          <Button style={styles.ctaButton} title="Show Modal" onPress={this.onShow} />
+          <Button
+            style={styles.ctaButton}
+            title="Show Modal"
+            onPress={this.onShow}
+          />
+          <FlatList
+            data={[
+              { title: "Title Text A", key: "item1" },
+              { title: "Title Text B", key: "item2" },
+              { title: "Title Text C", key: "item3" },
+              { title: "Title Text D", key: "item4" },
+              { title: "Title Text E", key: "item5" },
+              { title: "Title Text F", key: "item6" }
+            ]}
+            renderItem={({ item, separators }) => (
+              <TouchableOpacity onPress={() => console.log(`Item: ${item.title}`)}>
+                <View style={{ backgroundColor: "white" }}>
+                  <Text>{item.title}</Text>
+                </View>
+              </TouchableOpacity>
+            )}
+          />
         </View>
         <Modal visible={this.state.visible} onDismiss={this.onDismiss} />
       </React.Fragment>
